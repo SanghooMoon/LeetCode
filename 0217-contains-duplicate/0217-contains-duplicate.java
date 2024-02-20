91369d03e98ccd759905666eb64da8aa74d4fcac
문상후 <sanghoo.moon@gmail.com>
2024-02-20 21:04:27 +0900

    Time: 7 ms (98.91%), Space: 61 MB (7.24%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> hset = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (!hset.add(nums[i])) {
+                return true;
+            }
+        }
+        
+        
+        return false;
+    }
+}
