9ac5521334a0575dff08a2732c2c37ea17a89c9f
문상후 <sanghoo.moon@gmail.com>
2024-03-06 21:04:34 +0900

    Time: 8 ms (79.23%), Space: 42.7 MB (16.50%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int sArr[] = new int[200];
+        int tArr[] = new int[200];
+
+        for(int i=0;i<s.length();i++) {
+            if(sArr[s.charAt(i)] != tArr[t.charAt(i)]) {
+                return false;
+            }
+            
+            sArr[s.charAt(i)]=i+1;
+            tArr[t.charAt(i)]=i+1;
+        }
+        return true;
+    }
+}
